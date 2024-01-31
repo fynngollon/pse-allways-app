ab0177acbb48e040159c65b7eb7037c48e134744
FelixM <muerdter.felix@gmail.com>
2024-01-31 18:21:42 +0100

    Added DefaultStatisticsRepository functions (therefore added suspend keywords in StatisticsRepository) and some minor changes to Trip and Stage (Amend to prev commit: Changed merge functions in DefaultStatisticsRepository.getModalSplitOfTrips to other functionality to function in Android 6.0)

--- data/src/main/java/com/pseteamtwo/allways/trip/Trip.kt
+++ data/src/main/java/com/pseteamtwo/allways/trip/Trip.kt
@@ -1,10 +1,12 @@
 package com.pseteamtwo.allways.trip
 
+import android.location.Location
 import org.osmdroid.util.GeoPoint
 import org.threeten.bp.Duration
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
+import java.time.LocalDateTime
 
 data class Trip(
     val id: String,
@@ -13,22 +15,22 @@ data class Trip(
     val isConfirmed: Boolean,
 ) {
 
-    val startDateTime
+    val startDateTime: LocalDateTime
         get() = stages.first().startDateTime
 
-    val endDateTime
+    val endDateTime: LocalDateTime
         get() = stages.last().endDateTime
 
-    val startLocation
+    val startLocation: Location
         get() = stages.first().startLocation
 
-    val endLocation
+    val endLocation: Location
         get() = stages.last().endLocation
 
-    val duration
+    val duration: Int
         get() = stages.sumOf { it.duration }
 
-    val distance
+    val distance: Int
         get() = stages.sumOf { it.distance }
 
 }
