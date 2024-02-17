f0ef533f524fcbcb195cd88a0a2253cdb2b40ae6
fynn Gollon <uxxxp@student.kit.edu>
2024-02-17 19:41:02 +0100

    stable build with ksp, room and hilt

---
+++ ui/src/main/java/com/pseteamtwo/allways/profile/QuestionUiState.kt
@@ -0,0 +1,6 @@
+package com.pseteamtwo.allways.profile
+
+data class QuestionUiState(
+    var id: String
+    // TODO
+)
---
+++ ui/src/main/java/com/pseteamtwo/allways/statistics/StatisticsViewModel.kt
@@ -0,0 +1,7 @@
+package com.pseteamtwo.allways.statistics
+
+import androidx.lifecycle.ViewModel
+
+class StatisticsViewModel : ViewModel() {
+    // TODO
+}
