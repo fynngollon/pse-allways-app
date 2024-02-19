57c308ae13d7748a321827b8a5ca0cb15d3b3d3b
FelixM <muerdter.felix@gmail.com>
2024-02-19 13:08:52 +0100

    Fixed type conflicts in DefaultStatisticsRepository.kt and therefore also change StatisticsRepository.kt Interface

--- data/src/main/java/com/pseteamtwo/allways/statistics/StatisticsRepository.kt
+++ data/src/main/java/com/pseteamtwo/allways/statistics/StatisticsRepository.kt
@@ -1,30 +1,30 @@
 package com.pseteamtwo.allways.statistics
 
 import com.pseteamtwo.allways.trip.Mode
-import java.time.LocalDateTime
-import java.util.Date
+import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
 import java.util.EnumMap
 
 interface StatisticsRepository {
 
     suspend fun getTripDistanceOfAll(): Int
 
-    suspend fun getTripDurationOfAll(): Int
+    suspend fun getTripDurationOfAll(): Long
 
     suspend fun getTripDistanceOfTimespan(startTime: LocalDateTime, endTime: LocalDateTime): Int
 
-    suspend fun getTripDurationOfTimespan(startTime: LocalDateTime, endTime: LocalDateTime): Int
+    suspend fun getTripDurationOfTimespan(startTime: LocalDateTime, endTime: LocalDateTime): Long
 
-    suspend fun getTripDistanceOfDate(date: Date): Int
+    suspend fun getTripDistanceOfDate(date: LocalDate): Int
 
-    suspend fun getTripDurationOfDate(date: Date): Int
+    suspend fun getTripDurationOfDate(date: LocalDate): Long
 
 
     suspend fun getAverageTripDistance(): Int
 
-    suspend fun getAverageTripDuration(): Int
+    suspend fun getAverageTripDuration(): Long
 
-    suspend fun getAverageTripSpeed(): Int
+    suspend fun getAverageTripSpeed(): Long
 
 
     suspend fun getModalSplitOfAll(percentaged: Boolean): EnumMap<Mode, Int>
@@ -37,6 +37,6 @@ interface StatisticsRepository {
 
     suspend fun getModalSplitOfDate(
         percentaged: Boolean,
-        date: Date
+        date: LocalDate
     ): EnumMap<Mode, Int>
 }
