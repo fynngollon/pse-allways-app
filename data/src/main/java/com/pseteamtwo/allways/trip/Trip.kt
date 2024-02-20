ddb42c70a89afc39e64a11ae3478600d43700bbf
FelixM <muerdter.felix@gmail.com>
2024-02-20 16:33:31 +0100

    Added Kdoc documentary for Trip, Stage, GpsPoint, Mode, Purpose, TripModelMapping and half of TripAndStageRepository

--- data/src/main/java/com/pseteamtwo/allways/trip/Trip.kt
+++ data/src/main/java/com/pseteamtwo/allways/trip/Trip.kt
@@ -7,13 +7,30 @@ import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 
+/**
+ * Representation of a trip traveled by the user (to be also used outside this data module).
+ *
+ * A trip can be created by recording through [com.pseteamtwo.allways.trip.tracking] tracking
+ * or by the user himself.
+ *
+ * @property id The unique identification number of the trip for saving, editing and retrieving
+ * purposes as well as for preventing duplicates.
+ * @property stages A list of [Stage]s of which the trip consists.
+ * If the trip is deleted, every of its [stages] should be deleted as well.
+ * @property purpose The [Purpose] the trip got traveled for.
+ * @property isConfirmed Indicates, whether the trip is confirmed by the user
+ * (trips created through tracking are not confirmed until the user confirms,
+ * while trips created by the user are confirmed).
+ * @constructor Creates a trip with the specified properties.
+ * The properties [startDateTime], [endDateTime], [startLocation], [endLocation], [duration]
+ * and [distance] are computed through the provided list of [Stage]s.
+ */
 data class Trip(
     val id: Long,
     val stages: List<Stage>,
     val purpose: Purpose,
     val isConfirmed: Boolean
 ) {
-
     val startDateTime: LocalDateTime
         get() = stages.first().startDateTime
 
--- data/src/main/java/com/pseteamtwo/allways/trip/repository/TripAndStageRepository.kt
+++ data/src/main/java/com/pseteamtwo/allways/trip/repository/TripAndStageRepository.kt
@@ -16,21 +16,80 @@ import org.threeten.bp.LocalDate
 import org.threeten.bp.LocalDateTime
 import kotlin.jvm.Throws
 
+/**
+ * Repository to handle all [Trip]s, [Stage]s and [GpsPoint]s and
+ * to provide according functionality for the ui-layer.
+ *
+ * This repository provides functionality to retrieve from and save to local and network databases
+ * as well as to modify this data of [Trip]s, [Stage]s and [GpsPoint]s while ensuring that these
+ * operations do not interfere with physical logic of time and space.
+ */
 interface TripAndStageRepository {
 
+    /**
+     * Retrieves all [Trip]s saved in the local trip database.
+     *
+     * @return A flow of all [Trip]s saved in the local trip database in form of a list
+     */
     suspend fun observeAllTrips(): Flow<List<Trip>>
 
+    /**
+     * Retrieves all [Stage]s belonging to the specified [Trip] from the local stage database.
+     *
+     * @param tripId Identification number of the [Trip], of which the [Stage]s are requested.
+     * @return All [Stage]s belonging to the specified [Trip].
+     */
     suspend fun observeStagesOfTrip(tripId: Long): Flow<List<Stage>>
 
+    /**
+     * Creates a new [Trip] with the provided parameters.
+     * Therefore creates a unique id for the new trip and saves it into the local trip database.
+     *
+     * @param stages The list of [Stage]s which the new trip consists of.
+     * @param purpose The purpose of the new trip.
+     *
+     * TODO("why isn't here a return: Trip as well as on [createGpsPoint]")
+     */
     suspend fun createTrip(stages: List<Stage>, purpose: Purpose)
 
     //suspend fun createStage(gpsPoints: List<GpsPoint>, mode: Mode): Stage
 
+    /**
+     * Creates a new [GpsPoint] with the provided [Location].
+     * Therefore creates a unique id for the new gpsPoint and saves it
+     * into the local gpsPoint database.
+     *
+     * @param location The [Location] which the new gpsPoint consists of.
+     * @return The created gpsPoint.
+     */
     suspend fun createGpsPoint(location: Location): GpsPoint
 
 
+    /**
+     * Updates the purpose of the specified [Trip].
+     *
+     * @param tripId Identification number of the [Trip], of which the purpose should be updated.
+     * @param purpose The new purpose.
+     */
     suspend fun updateTripPurpose(tripId: Long, purpose: Purpose)
 
+    /**
+     * Updates every property of the specified [Stage] except [Stage.id] and [Stage.gpsPoints].
+     * Because the updated properties aren't computed out of [Stage.gpsPoints] anymore, this list
+     * should be set to exactly 2 [GpsPoint]s each containing a [Location] composed out of
+     * startTime and startLocation respectively endTime and endLocation.
+     * Also ensures, that this updating does not interfere with physical logic of time.
+     *
+     * @param stageId Identification number of the [Stage].
+     * @param mode The new [Mode].
+     * @param startTime The new start time.
+     * @param endTime The new end time.
+     * @param startLocation The new start location.
+     * @param endLocation The new end location.
+     * @throws NoTimeContinuityException If the provided parameters interfere with physical logic
+     * of time, e.g. if the new [startTime] would be during another [Stage] of the
+     * local stage database temporally.
+     */
     @Throws(NoTimeContinuityException::class)
     suspend fun updateStage(
         stageId: Long,
