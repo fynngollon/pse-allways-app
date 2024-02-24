1ef49501161ca0d7a32bb2a6dc4f14c95e3f0775
FelixM <muerdter.felix@gmail.com>
2024-02-24 14:49:16 +0100

    Deletion of QuestionnaireUtils.kt and a function in DefaultQuestionRepository.kt

--- data/src/main/java/com/pseteamtwo/allways/question/QuestionType.kt
+++ data/src/main/java/com/pseteamtwo/allways/question/QuestionType.kt
@@ -2,10 +2,29 @@ package com.pseteamtwo.allways.question
 
 import kotlinx.serialization.Serializable
 
+/**
+ * Defines the answer type of a [Question].
+ * A [Question] has exactly one [QuestionType].
+ */
 @Serializable
 enum class QuestionType {
+    /**
+     * The [Question] can be answered through a text insertion.
+     */
     TEXT,
+
+    /**
+     * The [Question] can be answered by choosing the option through a radio button.
+     */
     RADIO_BUTTON,
+
+    /**
+     * The [Question] can be answered by choosing the option through a checked checkbox.
+     */
     CHECKBOX,
+
+    /**
+     * The [Question] can be answered by choosing the option through a spinner.
+     */
     SPINNER
 }
