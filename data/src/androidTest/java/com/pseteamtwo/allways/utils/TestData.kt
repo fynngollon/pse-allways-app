bd63b8ff7f6dacdda515f1b6bb385a8f728f0114
FelixM <muerdter.felix@gmail.com>
2024-02-24 01:40:22 +0100

    Fixed test DefaultTripAndStageRepositoryTest.kt

--- data/src/androidTest/java/com/pseteamtwo/allways/utils/TestData.kt
+++ data/src/androidTest/java/com/pseteamtwo/allways/utils/TestData.kt
@@ -4,8 +4,9 @@ import android.location.Location
 import com.pseteamtwo.allways.trip.GpsPoint
 import com.pseteamtwo.allways.trip.Mode
 import com.pseteamtwo.allways.trip.Stage
+import com.pseteamtwo.allways.trip.convertToLocalDateTime
 import org.osmdroid.util.GeoPoint
-
+import org.threeten.bp.LocalDateTime
 
 
 val geoPoint1 = GeoPoint(0.000, 0.001)
@@ -13,20 +14,22 @@ val geoPoint2 = GeoPoint(0.000, 0.002)
 val geoPoint3 = GeoPoint(0.000, 0.002)
 val geoPoint4 = GeoPoint(0.000, 0.003)
 
-val location1 = geoPoint1.toLocation(0)
-val location2 = geoPoint2.toLocation(10)
-val location3 = geoPoint3.toLocation(10)
-val location4 = geoPoint4.toLocation(30)
+const val TIME_1: Long = 0
+const val TIME_2: Long = 10
+const val TIME_3: Long = 20
+const val TIME_4: Long = 50
 
 val stage1 = Stage(
     1,
     Mode.WALK,
-    listOf(GpsPoint(1, location1), GpsPoint(2, location2))
+    listOf(GpsPoint(1, geoPoint1, TIME_1.convertToLocalDateTime()),
+        GpsPoint(2, geoPoint2, TIME_2.convertToLocalDateTime()))
 )
 val stage2 = Stage(
     2,
     Mode.MOTORCYCLE,
-    listOf(GpsPoint(3, location3), GpsPoint(4, location4))
+    listOf(GpsPoint(3, geoPoint3, TIME_3.convertToLocalDateTime()),
+        GpsPoint(4, geoPoint4, TIME_4.convertToLocalDateTime()))
 )
 
 
