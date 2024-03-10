72bbfd0f566adb73c3d8fc62a08c795e977c6642
FelixM <muerdter.felix@gmail.com>
2024-03-10 16:26:27 +0100

    Maybe fixed DefaultTripAndStageRepository.updateStage

--- data/src/main/java/com/pseteamtwo/allways/trip/repository/DefaultTripAndStageRepository.kt
+++ data/src/main/java/com/pseteamtwo/allways/trip/repository/DefaultTripAndStageRepository.kt
@@ -466,14 +466,17 @@ class DefaultTripAndStageRepository @Inject constructor(
             || !endLocation.compareTo(endLocationOfLocalStage)
             || startLocationOfLocalStage.time != startTimeMillis
             || endLocationOfLocalStage.time != endTimeMillis) {
+            //create new start and end gpsPoint (inserted into database and assigned to localStage)
+            val newStartGpsPoint = createGpsPoint(startLocation.toLocation(startTimeMillis))
+            gpsPointLocalDataSource.update(newStartGpsPoint.copy(stageId = stageId))
+            val newEndGpsPoint = createGpsPoint(endLocation.toLocation(endTimeMillis))
+            gpsPointLocalDataSource.update(newEndGpsPoint.copy(stageId = stageId))
+
             //start or end location or start or end time have changed
             //delete all former GpsPoints of the stage
             gpsPointsOfLocalStage.forEach { localGpsPoint ->
                 gpsPointLocalDataSource.delete(localGpsPoint.id)
             }
-            //create new start and end gpsPoint (inserted into database and assigned to localStage)
-            createGpsPoint(startLocation.toLocation(startTimeMillis))
-            createGpsPoint(endLocation.toLocation(endTimeMillis))
         }
         //update mode of localStage in database
         stageLocalDataSource.update(localStage)
