2c2da821903fa8623860333da95eff711d514bac
fynng <pilsygaming@gmail.com>
2024-03-14 21:42:48 +0100

    integrated tracking algorithm start pt.2

--- data/src/main/java/com/pseteamtwo/allways/data/trip/tracking/TrackingAlgorithmManager.kt
+++ data/src/main/java/com/pseteamtwo/allways/data/trip/tracking/TrackingAlgorithmManager.kt
@@ -8,15 +8,19 @@ class TrackingAlgorithmManager @Inject constructor(
     private val trackingAlgorithm: DefaultTrackingAlgorithm
 ) {
 
-    private var timestampLastMotionless = 0L
+    private var timestampLastMotionless = 100L
 
     fun requestAlgorithm(lastLocation: Location) {
         Log.d("PSE_TRACKING", "TrackingAlgorithmManager: requesting to start algorithm")
         if (lastLocation.speed < DefaultTrackingAlgorithm.STILL_MOTION_THRESHOLD) {
+            Log.d("PSE_TRACKING", "TrackingAlgorithmManager: Speed under threshold")
             Log.d("PSE_TRACKING", "TrackingAlgorithmManager: Timestamp: $timestampLastMotionless")
+            Log.d("PSE_TRACKING", "TrackingAlgorithmManager: Time elapsed: ${lastLocation.time - timestampLastMotionless}")
             if (timestampLastMotionless == 0L) {
+                Log.d("PSE_TRACKING", "TrackingAlgorithmManager: Set timestamp")
                 timestampLastMotionless = lastLocation.time
             } else if (timestampLastMotionless + DefaultTrackingAlgorithm.MAX_MOTIONLESS_IN_TRIP < lastLocation.time) {
+                Log.d("PSE_TRACKING", "TrackingAlgorithmManager: 15min motionless elapsed. Starts algorithm now")
                 trackingAlgorithm.observeTrackingData()
                 timestampLastMotionless = 0L
             }
