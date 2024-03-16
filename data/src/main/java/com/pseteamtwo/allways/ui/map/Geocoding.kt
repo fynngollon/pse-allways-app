dc84333140ebbcea3cfea3ae9ffefcff26c217d1
FelixM <muerdter.felix@gmail.com>
2024-03-16 19:31:22 +0100

    lol

--- data/src/main/java/com/pseteamtwo/allways/ui/map/Geocoding.kt
+++ data/src/main/java/com/pseteamtwo/allways/ui/map/Geocoding.kt
@@ -1,9 +1,6 @@
 package com.pseteamtwo.allways.ui.map
 
 import android.location.Address
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlin.coroutines.CoroutineContext
 
 fun addressToString(address: Address): String {
     return (address.thoroughfare ?: "") + " " + (address.subThoroughfare ?: "") + ", " + /*(address.postalCode ?: "" ) + " " +*/ (address.locality ?: "")
