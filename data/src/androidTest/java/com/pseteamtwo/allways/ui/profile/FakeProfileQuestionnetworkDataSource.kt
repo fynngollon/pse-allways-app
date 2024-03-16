f943e4de4285ac3a46d437d0ad34c3679501d781
Lorenz_laha <uqhpf@student.kit.edu>
2024-03-16 14:16:57 +0100

    added tests for tripsviewmodel

---
+++ data/src/androidTest/java/com/pseteamtwo/allways/ui/profile/FakeProfileQuestionnetworkDataSource.kt
@@ -0,0 +1,6 @@
+package com.pseteamtwo.allways.ui.profile
+
+import com.pseteamtwo.allways.data.question.source.network.ProfileQuestionNetworkDataSource
+
+class FakeProfileQuestionnetworkDataSource {
+}
